fc500bc18728ab7fb9f21be3e3641378bda98161
muhammadmujtabadurani <145250058+muhammadmujtabadurani@users.noreply.github.com>
2024-01-06 00:36:38 -0800

    Add files via upload

---
+++ one.js
@@ -0,0 +1,20 @@
+let products = [
+    {
+        "id": 0,
+        "name": "Haseeb",
+        "age": 16,
+        "class":"11-cs",
+    },
+    {
+        "id": 1,
+        "name": "Mujtaba",
+        "age": 17,
+        "class":"11-cs",
+    },
+    {
+        "id": 2,
+        "name": "Hassan",
+        "age": 18,
+        "class":"12-cs",
+    },
+]
