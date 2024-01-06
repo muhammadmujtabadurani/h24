5f30b06b5634e4b02b7058d430429d237a9e00b0
muhammadmujtabadurani <145250058+muhammadmujtabadurani@users.noreply.github.com>
2024-01-06 07:22:57 -0800

    Add files via upload

--- one.js
+++ one.js
@@ -1,20 +1,29 @@
-let products = [
-    {
-        "id": 0,
-        "name": "Haseeb",
-        "age": 16,
-        "class":"11-cs",
-    },
-    {
-        "id": 1,
-        "name": "Mujtaba",
-        "age": 17,
-        "class":"11-cs",
-    },
-    {
-        "id": 2,
-        "name": "Hassan",
-        "age": 18,
-        "class":"12-cs",
-    },
-]
+for(let i=1; i<products.length; i++){
+    document.getElementById('select1').innerHTML += `
+    <option value="${i}">${products[i].title}</option>
+    `
+
+
+}
+
+for(let i=1; i<products.length; i++){
+    document.getElementById('select2').innerHTML += `
+    <option value="${i}">${products[i].title}</option>
+    `
+
+
+}
+
+function item1(a){
+    document.getElementById('img1').src = products[a].image;
+    document.getElementById('price1').innerHTML = products[a].price;
+    document.getElementById('brand1').innerHTML = products[a].brand;
+    document.getElementById('desc1').innerHTML = products[a].description;
+}
+
+function item2(a){
+    document.getElementById('img2').src = products[a].image;
+    document.getElementById('price2').innerHTML = products[a].price;
+    document.getElementById('brand2').innerHTML = products[a].brand;
+    document.getElementById('desc2').innerHTML = products[a].description;
+}
